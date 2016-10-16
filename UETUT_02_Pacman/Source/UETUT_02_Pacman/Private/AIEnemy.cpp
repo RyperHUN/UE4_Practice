1f35ddbc4e15ae84b7bd0288b0690285545e378c
RyperHUN <akormanyzsolt@gmail.com>
2016-10-16 22:50:56 +0200

    Added AIEnemy logic

--- UETUT_02_Pacman/Source/UETUT_02_Pacman/Private/AIEnemy.cpp
+++ UETUT_02_Pacman/Source/UETUT_02_Pacman/Private/AIEnemy.cpp
@@ -21,24 +21,46 @@ AAIEnemy::AAIEnemy()
 void AAIEnemy::OnMoveCompleted(FAIRequestID RequestId,
 	EPathFollowingResult::Type Result)
 {
+	if(!Bot->bIsDead)
+		SearchNewPoint ();
 }
 
+// in order to find a new point we search if there is a navigation
+//mesh active and
+// when found, we simple call the GetRandomPointInRadius function from
+//the NavMesh
 void AAIEnemy::SearchNewPoint()
 {
-
+	UNavigationSystem* NavMesh =
+		UNavigationSystem::GetCurrent(this);
+	if (NavMesh)
+	{
+		const float SearchRadius = 10000.0f;
+		FNavLocation RandomPt;
+		const bool bFound = NavMesh->GetRandomPointInNavigableRadius
+		(Bot->GetActorLocation(), SearchRadius, RandomPt);
+		if (bFound)
+		{
+			MoveToLocation(RandomPt.Location);
+		}
+	}
 }
-
+// simple return to the home location and start a timer for the dead
 void AAIEnemy::GoHome()
 {
+	MoveToLocation(HomeLocation);
+	GetWorldTimerManager().SetTimer(TimerDead, this, &AAIEnemy::Rearm, 5.0f, false);
 }
-
+//Almost like Respawn
 void AAIEnemy::Rearm()
 {
+	GetWorldTimerManager().ClearTimer(TimerDead);
+	Bot->Rearm();
 }
 
 void AAIEnemy::StopMove()
 {
-
+	StopMovement();
 }
 
 
